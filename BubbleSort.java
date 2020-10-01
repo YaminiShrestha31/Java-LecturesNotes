1f8a84e64d48f26995730e50b28bfacae26272dd
codetravellearn <43231383+codetravellearn@users.noreply.github.com>
2020-10-01 20:47:16 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,37 +1,70 @@
-// Java program for implementation of Bubble Sort 
-class BubbleSort 
-{ 
-	void bubbleSort(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i = 0; i < n-1; i++) 
-			for (int j = 0; j < n-i-1; j++) 
-				if (arr[j] > arr[j+1]) 
-				{ 
-					// swap arr[j+1] and arr[j] 
-					int temp = arr[j]; 
-					arr[j] = arr[j+1]; 
-					arr[j+1] = temp; 
-				} 
-	} 
+import java.util.Arrays;
+import java.util.Scanner;
 
-	/* Prints the array */
-	void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i] + " "); 
-		System.out.println(); 
-	} 
+class Main {
 
-	// Driver method to test above 
-	public static void main(String args[]) 
-	{ 
-		BubbleSort ob = new BubbleSort(); 
-		int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
-		ob.bubbleSort(arr); 
-		System.out.println("Sorted array"); 
-		ob.printArray(arr); 
-	} 
-} 
+  // create an object of scanner
+  // to take input from the user
+  Scanner input = new Scanner(System.in);
 
+  // method to perform bubble sort
+  void bubbleSort(int array[]) {
+    int size = array.length;
+
+    // for ascending or descending sort
+    System.out.println("Choose Sorting Order:");
+    System.out.println("1 for Ascending \n2 for Descending");
+    int sortOrder = input.nextInt();
+
+    // run loops two times
+    // first loop access each element of the array
+    for (int i = 0; i < size - 1; i++)
+
+      // second loop performs the comparison in each iteration
+      for (int j = 0; j < size - i - 1; j++)
+
+        // sort the array in ascending order
+        if (sortOrder == 1) {
+          // compares the adjacent element
+          if (array[j] > array[j + 1]) {
+
+            // swap if left element is greater than right
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+
+        // sort the array in descending order
+        else {
+          // compares the adjacent element
+          if (array[j] < array[j + 1]) {
+
+            // swap if left element is smaller than right
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+          }
+        }
+
+  }
+
+  // driver code
+  public static void main(String args[]) {
+
+    // create an array
+    int[] data = { -2, 45, 0, 11, -9 };
+
+    // create an object of Main class
+    Main bs = new Main();
+
+    // call the method bubbleSort using object bs
+    // pass the array as the method argument
+    bs.bubbleSort(data);
+    System.out.println("Sorted Array in Ascending Order:");
+
+    // call toString() of Arrays class
+    // to convert data into the string
+    System.out.println(Arrays.toString(data));
+  }
+}
