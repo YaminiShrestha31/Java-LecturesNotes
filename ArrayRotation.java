ca56d1bc1f965215a32b7414c3426c5444c2371c
Restyled.io <commits@restyled.io>
2020-10-02 09:12:34 +0000

    Restyled by clang-format

--- ArrayRotation.java
+++ ArrayRotation.java
@@ -1,58 +1,50 @@
 import java.util.*;
-class Main 
-{
-    void rightRotate(int[] arr, int n)
-    {
-        int temp;
-        for (int i = 1; i <= n; i++) 
-            { 
-            temp = arr[arr.length-1]; 
-            for (int j = arr.length-1; j > 0; j--) 
-            {
-                arr[j] = arr[j-1];
-            }
-            arr[0] = temp;
-        }
-        System.out.println("Input Array After Right Rotation By "+n+" Positions :");
-        System.out.println(Arrays.toString(arr));
+class Main {
+  void rightRotate(int[] arr, int n) {
+    int temp;
+    for (int i = 1; i <= n; i++) {
+      temp = arr[arr.length - 1];
+      for (int j = arr.length - 1; j > 0; j--) {
+        arr[j] = arr[j - 1];
+      }
+      arr[0] = temp;
     }
-    
-    void leftRotate(int[] arr, int n) 
-    {
-        int temp;
-        for (int i = 0; i < n; i++)
-        {
-            temp = arr[0];
-            for (int j = 0; j < arr.length-1; j++) 
-            {
-                arr[j] = arr[j+1];
-            }
-            arr[arr.length - 1] = temp;
-        }
-        System.out.println("Input Array After Left Rotation By "+n+" Positions :");
-        System.out.println(Arrays.toString(arr));
+    System.out.println("Input Array After Right Rotation By " + n +
+                       " Positions :");
+    System.out.println(Arrays.toString(arr));
+  }
+
+  void leftRotate(int[] arr, int n) {
+    int temp;
+    for (int i = 0; i < n; i++) {
+      temp = arr[0];
+      for (int j = 0; j < arr.length - 1; j++) {
+        arr[j] = arr[j + 1];
+      }
+      arr[arr.length - 1] = temp;
     }
-     
-    public static void main(String[] args) 
-    {
-        Scanner s= new Scanner(System.in);
-        System.out.println("Enter the length of the array you wish to rotate: ");
-        int l=s.nextInt();
-        int arr1[]=new int[l];
-        int arr2[]=new int[l];
-        System.out.println("Enter the Elements");
-        for(int i=0 ; i < l; i++)
-        {
-            arr1[i]=s.nextInt();
-            arr2[i]=arr1[i];
-        }
-        System.out.println("Enter the index of rotation");
-        int r=s.nextInt();
-        System.out.println("Input Array Before Rotation :");
-        System.out.println(Arrays.toString(arr1));
-        Main m= new Main();
-        m.rightRotate(arr1, r);
-        m.leftRotate(arr2, r);
-        
+    System.out.println("Input Array After Left Rotation By " + n +
+                       " Positions :");
+    System.out.println(Arrays.toString(arr));
+  }
+
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    System.out.println("Enter the length of the array you wish to rotate: ");
+    int l = s.nextInt();
+    int arr1[] = new int[l];
+    int arr2[] = new int[l];
+    System.out.println("Enter the Elements");
+    for (int i = 0; i < l; i++) {
+      arr1[i] = s.nextInt();
+      arr2[i] = arr1[i];
     }
+    System.out.println("Enter the index of rotation");
+    int r = s.nextInt();
+    System.out.println("Input Array Before Rotation :");
+    System.out.println(Arrays.toString(arr1));
+    Main m = new Main();
+    m.rightRotate(arr1, r);
+    m.leftRotate(arr2, r);
+  }
 }
